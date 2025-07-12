227341040482bd96861f25baa6dbd5afd320ee9c
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-12 18:25:17 +0530

    added context value and value pass for children provider wrap

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,23 +1,27 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    consr [user,setUser]= useState(()=> {
-  const stored = localStorage.getItem("user");
-  return stored ? JSON.parse(stored) : null;
-    });
-    
-    const login = (userData, token) => {
-        localStorage.setItem("user", JSON.stringify(userData));
-        localStorage.setItem("token", token)
-        setUser(userData);
-    };
+  consr[(user, setUser)] = useState(() => {
+    const stored = localStorage.getItem("user");
+    return stored ? JSON.parse(stored) : null;
+  });
 
-    const logout = () => {
-        localStorage.removeItem("user")
-        localStorage.removeItem("token")
-        setUser(null)
-    }
+  const login = (userData, token) => {
+    localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("token", token);
+    setUser(userData);
+  };
 
-export default AuthContext;
+  const logout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  const value = { user, login, logout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+export const userAuth = () => useContext(AuthContext);
