a8d43e1b7b00cdcfe59ff7f31e6a48b7f1b03777
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-12 17:39:26 +0530

    added provide component and login fucntion

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    consr [user,setUser]= useState(()=> {
+  const stored = localStorage.getItem("user");
+  return stored ? JSON.parse(stored) : null;
+    });
+    
+    const login = (userData, token) => {
+        localStorage.setItem("user", JSON.stringify(userData));
+        localStorage.setItem("token", token)
+        setUser(userData);
+    };
+
+export default AuthContext;
