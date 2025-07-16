897af1a3f1a10e7ae9175ac4f650d3b678bbb50b
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-16 16:03:38 +0530

    implemented user login logic with form state, error handling, and navigation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { userAuth } from "../context/AuthContext";
+import { loginRequest } from "../services/authService";
 
-const Login = () => {
+function Login() {
+  const [form, setForm] = useState({ username: "", password: "" });
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+  const { login } = userAuth();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefalut();
+    setError(null);
+
+    try {
+      const data = await loginRequest(form);
+      login({ id: data.id, username: data.username }, data.token);
+      navigate("/dashboard");
+    } catch (err) {
+      setError(err.resposne?.data?.message || "Login Faild");
+    }
+  };
   return (
     <div className="login-container">
       <h2>Login</h2>
@@ -17,6 +41,6 @@ const Login = () => {
       </form>
     </div>
   );
-};
+}
 
 export default Login;
